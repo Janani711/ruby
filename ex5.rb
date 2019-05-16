029858c8cca891ae688201723df3b6399c9257d1
Janani711 <41939542+Janani711@users.noreply.github.com>
2019-05-16 15:17:19 +0530

    Add files via upload

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Zed A.Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He is #{my_height} inches tall."
+puts "He is #{my_weight} pound heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "his teeth are usually #{my_teeth} depending on the coffee."
+
+
+puts "if i add #{my_age}, #{my_height}  and #{my_weight} i get #{my_age + my_height + my_weight}."
